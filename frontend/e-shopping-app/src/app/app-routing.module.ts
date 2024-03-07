68c1e957eed4fbec827a577378fff25db9d496e2
kieunguyenus89 <kieunguyenus89@gmail.com>
2024-03-07 15:19:37 -0500

    initailize

---
+++ frontend/e-shopping-app/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { CustomerhomeComponent } from './customerhome/customerhome.component';
+
+
+const routes: Routes = [
+  {path:"signIn",component:LoginComponent},
+  {path:"signUp",component:SignupComponent},
+  {path:"adminhome",component:AdminhomeComponent,children:[
+   
+  ]},
+
+  {path:"customerhome",component:CustomerhomeComponent,children:[
+   
+  ]},
+  
+  {path:"",redirectTo:"signIn",pathMatch:"full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/e-shopping-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { CustomerhomeComponent } from './customerhome/customerhome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    AdminhomeComponent,
+    CustomerhomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
